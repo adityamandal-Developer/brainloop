dc25a92ed679baceec20cb61e2437a4c53eb4a2a
adityamandal-Developer <mandaladitya901@gmail.com>
2025-07-04 17:18:42 +0530

    first commit

---
+++ api/src/server.ts
@@ -0,0 +1,22 @@
+import fastify from "fastify";
+import { PrismaClient } from "./generated/prisma";
+
+const prisma = new PrismaClient();
+const server = fastify({
+  logger: true,
+});
+
+server.get("/ping", async (request, reply) => {
+  return reply.send({
+    message: "pong",
+    status: 200,
+  });
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
